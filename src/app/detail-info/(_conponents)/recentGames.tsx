3eb52c4a396d772790d1aea1f0e4a219b80223f8
sorlros <sorlros2047@gmail.com>
2024-05-28 16:03:50 +0900

    챔피언 이미지 추가 및 최근 게임 추가

--- src/app/detail-info/(_conponents)/recentGames.tsx
+++ src/app/detail-info/(_conponents)/recentGames.tsx
@@ -1,15 +1,86 @@
-import { Match } from "@/types/types";
+import { Info, Match, MetaData } from "@/types/types";
+import { useEffect, useState } from "react";
+import Image from 'next/image';
 
 interface RecentGamesProps {
-  matchInfos: Match[];
+  matchInfos: Match[]
 }
 
 export const RecentGames = ({matchInfos}: RecentGamesProps) => {
+  const [isOpen, setIsOpen] = useState();
+
+  useEffect(() => {
+    if (matchInfos.length > 0 && matchInfos[0].info.participants.length > 0) {
+      console.log("AADD", matchInfos[0].info.participants[0].championName);
+    }
+  }, [matchInfos]);
+
   return (
-    <div className="flex w-2/3 min-h-screen rounded-lg bg-[#28344E]">
+    <div className="flex flex-col w-2/3 min-h-screen rounded-lg bg-[#28344E]">
+      {matchInfos.map((matchInfo, idx) => (
+        <div key={idx} className="flex w-full h-[96px] rounded-lg py-[4px] mb-4">
+          <div className="flex flex-col w-[110px] h-full">
+            <span>{matchInfo.info.gameMode}</span>
+            <span>몇일전</span>
+            <span>승패</span>
+            <span>게임 시간</span>
+          </div>
+          <div className="flex flex-col w-3/5 h-full">
+            <div className="flex w-full h-4/5">
+              <div>
+                <Image 
+                  src={`https://ddragon.leagueoflegends.com/cdn/14.10.1/img/champion/${matchInfo.info.participants[0].championName}.png`}
+                  alt="챔피언 이미지"
+                  width={48}
+                  height={48}
+                />
+              </div>
+              <div>
+                <div>스펠1</div>
+                <div>스펠2</div>
+              </div>
+              <div>
+                <div>주룬</div>
+                <div>부룬</div>
+              </div>
+              <div>
+                <span>킬</span>
+                <span>어시</span>
+                <span>데스</span>
+              </div>
+              <div>
+                <div>킬관여율</div>
+                <div>제어와드</div>
+                <div>cs(분당cs)</div>
+                <div>티어</div>
+              </div>
+            </div>
+            
+            <div className="flex flex-col w-full h-1/5">
+              <div className="flex">
+                <div>아이템 목록 & 장신구</div>
+                <div>최대킬 , 스코어</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-1/5 h-full">
+            <div>
+              사람목록
+            </div>
+          </div>
+          <div>
+            <div>
+              버튼 위치
+            </div>
+          </div>
+        </div>
+      ))}
+      
+
+      
       <div className="py-2">
         {matchInfos.map((matchInfo, index) => (
-          <div key={index}>
+          <div key={index}> 
             <h2>Match {index + 1}</h2>
             <p>Match ID: {matchInfo.metadata.matchId}</p>
             <p>Game Mode: {matchInfo.info.gameMode}</p>
